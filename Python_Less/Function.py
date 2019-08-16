358102b06eedfeae7faf6eb56e186941b6e4ec55
DPavlukhin <nekr-mapper@mail.ru>
2019-08-16 17:36:11 +0500

    Update Function.py
    UPD

--- Python_Less/Function.py
+++ Python_Less/Function.py
@@ -4,6 +4,33 @@ import os
 import psutil
 import sys
 
+num = 1
+endnum = 10
+answer = ""
+
+def degr():
+    number = int(input("Введите число >>>   "))
+    degree = int(input("Введите степень >>>   "))
+    summary = number ** degree
+    print(summary)
+
+
+
 polosa = "------------------"
 print("Это новая Прога - DAriec")
 
+
+
+
+
+
+while answer != "q":
+    degr()
+
+
+
+
+
+#while num != endnum:
+#    print(num, " - из - ", endnum)
+#    num += 1
