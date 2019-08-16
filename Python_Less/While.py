986f4b5305b0d7aba18046f9eb7544bebeb98475
DPavlukhin <nekr-mapper@mail.ru>
2019-08-16 17:03:47 +0500

    While-Example
    New File with example @WHILE@

---
+++ Python_Less/While.py
@@ -0,0 +1,30 @@
+# coding : utf-8
+
+import os
+import psutil
+import sys
+import random
+
+
+polosa = "------------------"
+print("Это новая Прога - DAriec")
+
+
+
+
+print("Введите IP адрес что бы выполнить PING")
+answer = input("Для примера >>  192.168.1.1 >>>   ")
+
+num = 0
+endnum = int(input("Введите количество пакетов >>>   "))
+
+print("Обмен пакетами с ", answer, " с 32 байтами данных:")
+
+while num != endnum:
+        rand = random.randint(40, 57)
+        num = num + 1
+        print("Ответ от ", answer, " число байт=32 время=", rand, "мс TTL=55")
+
+        if num == endnum:
+            print("Статистика Ping для ", answer)
+            print("Пакетов: отправлено = ", num, ", получено =", num, ", потеряно = ", 0)
