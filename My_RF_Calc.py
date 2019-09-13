e54f82d27d2d4c557e4379698674d89f80367b69
DPavlukhin <nekr-mapper@mail.ru>
2019-09-13 10:16:18 +0500

    Update My_RF_Calc.py
    Version 1.0

--- My_RF_Calc.py
+++ My_RF_Calc.py
@@ -1,19 +1,38 @@
 # coding: utf-8
+import math
 
+print("        ")
+print("РадиоКалькулятор 1.0v")
+print("Автор: Дмитрий Павлухин")
 
 
 dBm = 0
-i = 'Y'
-while i != 'N':
+work = 0
+answer = 'Y'
 
-    if i == 'Y':
+while answer != 'Q':
+    print("        ")
+    print('Какую команду мне выполнить?')
+    print('1 - Перевод из dBm в W')
+    print('2 - Перевод из W в dBm')
+    print('Q - Выйти из программы.')
+
+    work = int(input(">>> "))
+
+    if work == 1:
         print("        ")
         dBm = int(input("Введите dBm >>>  "))
         a = dBm / 10
         b = 10 ** a
         c = b / 1000
-
         print("Введено: ", dBm, "dBm")
-        print("МиллиВатт: ","%.3f" % b, "mW")
-        print("Ватт: ","%.4f" % c, "W")
+        print("МиллиВатт: ", "%.3f" % b, "mW")
+        print("Ватт: ", "%.4f" % c, "W")
 
+
+
+    if work == 2:
+        print("        ")
+        W = int(input("Введите W >>>  "))
+        print("Введено: ", W, "W")
+        print("Децибел-Вт: ", "Тут потом будет еще инфа")
